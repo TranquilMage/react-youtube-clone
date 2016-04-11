42f031024f5034d47c6beb839079137a99dca744
Triggs <triggs@webjhest.com>
2016-04-10 21:39:42 -0400

    init commit react app

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SearchBar} from './search_bar'
+export { default as VideoDetail} from './video_details'
+export { default as VideoList} from './video_list'
+export { default as VideoLisitem} from './video_list_item'
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+
+import { SearchBar, VideoList, VideoDetail } from './components';
+
+const API_KEY = 'AIzaSyCUH_KsX_cyVXj0thwYcib9VdO2mXSm_EU';
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+    this.videoSearch('forex')
+
+	}
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); // this.setState({videos: videos})
+    });
+
+  }
+	render() {
+    const videoSearch = _.debounce((term)=> {this.videoSearch(term)}, 300);
+			return (
+				<div>
+					<SearchBar onSearchTermChange={videoSearch} />
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList
+						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+						videos={this.state.videos} />
+				</div>
+			)
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
